9d48e3bfdcbe6260c99c26a34aeb6aed70947a2d
Nathan Evans <evans.nathan.j@gmail.com>
2019-07-29 12:46:05 -0600

    Use sentry sdk instead of raven

--- django_q_sentry/sentry.py
+++ django_q_sentry/sentry.py
@@ -1,10 +1,10 @@
-import raven
+import sentry_sdk
 
 
 class Sentry(object):
 
     def __init__(self, dsn, **kwargs):
-        self.client = raven.Client(dsn, **kwargs)
+        self.client = sentry_sdk.init(dsn=dsn, **kwargs)
 
     def report(self):
-        self.client.captureException()
+        sentry_sdk.capture_exception()
