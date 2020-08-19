c37cdb25c00cb901c82d6b4d79eefb8c2840b0bb
Christo Goosen <christogoosen@gmail.com>
2020-08-19 16:09:27 +0200

    Update package version

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ except ImportError:
 
 setup(
     name='django-q-sentry',
-    version='0.1.2',
-    author='Daniel Welch',
-    author_email='dwelch2102@gmail.com',
+    version='0.1.3',
+    author='Daniel Welch, Christo Goosen',
+    author_email='dwelch2102@gmail.com, christogoosen@gmail.com',
     keywords='django distributed task queue worker scheduler cron redis disque ironmq sqs orm mongodb multiprocessing sentry',
     packages=['django_q_sentry'],
-    install_requires=['sentry-sdk>=0.1.0'],
+    install_requires=['sentry-sdk>=0.16.5'],
     url='https://django-q.readthedocs.org',
     license='MIT',
     description='A Sentry support plugin for Django Q',
